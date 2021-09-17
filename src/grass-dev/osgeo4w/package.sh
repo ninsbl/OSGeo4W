5b725de79cf243e493feecb7b62e3252caae61c1
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-17 19:21:31 +0200

    cd

--- src/grass-dev/osgeo4w/package.sh
+++ src/grass-dev/osgeo4w/package.sh
@@ -4,7 +4,9 @@ export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="gdal-devel proj-devel geos-devel libjpeg-devel libpng-devel libpq-devel libtiff-devel sqlite3-devel zstd-devel python3-core python3-six python3-pywin32 liblas-devel python3-wxpython"
 
-source /d/a/OSGeo4W/OSGeo4W/scripts/build-helpers
+cd /d/a/OSGeo4W/OSGeo4W/src/grass-dev/osgeo4w
+
+source ../../../scripts/build-helpers
 
 set -o | grep -s "xtrace[	 ]*on" && xtrace=-x || true
 
