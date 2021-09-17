f8ad39fa9378dd54b43f691f633340f19693c0e9
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-17 19:50:39 +0200

    set REP

--- src/grass-dev/osgeo4w/package.sh
+++ src/grass-dev/osgeo4w/package.sh
@@ -4,6 +4,8 @@ export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="gdal-devel proj-devel geos-devel libjpeg-devel libpng-devel libpq-devel libtiff-devel sqlite3-devel zstd-devel python3-core python3-six python3-pywin32 liblas-devel python3-wxpython"
 
+export OSGEO4W_REP=/d/a/OSGeo4W/OSGeo4W/src/grass-dev/osgeo4w
+
 cd /d/a/OSGeo4W/OSGeo4W/src/grass-dev/osgeo4w
 
 source ../../../scripts/build-helpers
