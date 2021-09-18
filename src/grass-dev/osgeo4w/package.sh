e16785b50b12b535308dc16122cdbceaf275d010
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-18 14:04:52 +0200

    set OSGeo4W_ROOT

--- src/grass-dev/osgeo4w/package.sh
+++ src/grass-dev/osgeo4w/package.sh
@@ -5,7 +5,7 @@ export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="gdal-devel proj-devel geos-devel libjpeg-devel libpng-devel libpq-devel libtiff-devel sqlite3-devel zstd-devel python3-core python3-six python3-pywin32 liblas-devel python3-wxpython"
 
 export OSGEO4W_REP=/d/a/OSGeo4W/OSGeo4W/src/grass-dev/osgeo4w
-export OSGEO4W_ROOT_MSYS="$(pwd)"
+export OSGEO4W_ROOT_MSYS="$OSGEO4W_REP"
 export OSGEO4W_ROOT=$(cygpath -w "$OSGEO4W_ROOT_MSYS")
 
 branch=previewbranch_8_0
