4b867770ccfc8dc623e5dd321259f1a0c750f438
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-19 00:57:00 +0200

    add branch

--- src/grass-dev/osgeo4w/package.sh
+++ src/grass-dev/osgeo4w/package.sh
@@ -4,6 +4,8 @@ export B=next
 export MAINTAINER=JuergenFischer
 export BUILDDEPENDS="gdal-devel proj-devel geos-devel libjpeg-devel libpng-devel libpq-devel libtiff-devel sqlite3-devel zstd-devel python3-core python3-six python3-pywin32 liblas-devel python3-wxpython"
 
+branch=previewbranch_8_0
+
 source /d/a/OSGeo4W/OSGeo4W/scripts/build-helpers
 
 set -o | grep -s "xtrace[	 ]*on" && xtrace=-x || true
