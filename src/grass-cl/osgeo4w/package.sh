600e52fc1f8a0476b721003fb78d3b070f799121
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-27 14:45:45 +0200

    clang 3

--- src/grass-cl/osgeo4w/package.sh
+++ src/grass-cl/osgeo4w/package.sh
@@ -49,36 +49,36 @@ echo "OSGEO4W_ROOT: $OSGEO4W_ROOT"
 
 echo "PATH: $PATH"
 
-echo "VCPATH"
+#echo "VCPATH"
 
 	export VCSDK="10.0.18362.0"
-	export VCPATH=$(
-		vs2019env
-		echo ${PATH//\/cygdrive/}
+#	export VCPATH=$(
+#		vs2019env
+#		echo ${PATH//\/cygdrive/}
 )
 
-	vs2019env
+#	vs2019env
 
-echo "VS160COMNTOOLS: $VS160COMNTOOLS"
-echo "LIB: $LIB"
-echo "INCLUDE: $INCLUDE"
-echo "PATH: $PATH"
-echo "VCPATH: $VCPATH"
+#echo "VS160COMNTOOLS: $VS160COMNTOOLS"
+#echo "LIB: $LIB"
+#echo "INCLUDE: $INCLUDE"
+#echo "PATH: $PATH"
+#echo "VCPATH: $VCPATH"
 
-	echo "fetchenv ./msvc-env.bat"
+#	echo "fetchenv ./msvc-env.bat"
 
-	fetchenv "./msvc-env.bat"
+#	fetchenv "./msvc-env.bat"
 
-echo "VS160COMNTOOLS: $VS160COMNTOOLS"
-echo "LIB: $LIB"
-echo "INCLUDE: $INCLUDE"
-echo "PATH: $PATH"
-echo "VCPATH: $VCPATH"
+#echo "VS160COMNTOOLS: $VS160COMNTOOLS"
+#echo "LIB: $LIB"
+#echo "INCLUDE: $INCLUDE"
+#echo "PATH: $PATH"
+#echo "VCPATH: $VCPATH"
 
-	echo "B"
+#	echo "B"
 
-echo "PATH: $PATH"
-export PATH="$VCPATH:$PATH"
+#echo "PATH: $PATH"
+#export PATH="$VCPATH:$PATH"
 export CXX="$(cygpath -m $CXX)"
 export CC="$(cygpath -m $CC)"
 
